09aa562916042db516a3482fb7e398a02e29e251
AllenOtto <kephamoturi@gmail.com>
2023-07-02 08:25:46 +0300

    Initial commit: Created a ClassDemo Project which is a demonstration of the workings of classes and objects in C#. Created two classes: Program and Staff

---
+++ ClassDemo/Staff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    internal class Staff
+    {
+        private string nameOfStaff;
+        private const int hourlyRate = 30;
+        private int hWorked;
+
+        public int MonthlySalary()
+        {
+            return hWorked * hourlyRate;
+        }
+    }
+}
