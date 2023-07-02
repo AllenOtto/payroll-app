8c3d3770b5bdeda2032ab4d525b24127fc662601
AllenOtto <kephamoturi@gmail.com>
2023-07-02 13:08:02 +0300

    Having added fields to the Staff class, I added getter and setter to the private hoursWorked field with an if statement that checks that the value entered by the user is not equal to zero

--- ClassDemo/Staff.cs
+++ ClassDemo/Staff.cs
@@ -12,6 +12,26 @@ namespace ClassDemo
         private const int hourlyRate = 30;
         private int hWorked;
 
+        public int HoursWorked
+        {
+            get { return hWorked;  }
+
+            set { 
+                if(value > 0)
+                {
+                    hWorked = value;
+
+                } else
+                {
+                    hWorked = 0;
+                }
+            }
+        }
+
+        // If you wanted to create getter and setter methods without the if checker, (just a plain getter and setter)
+        // The following shortcut option would have sufficed instead of the block of code above
+        // public int HoursWorked { get; set; }
+
         public int MonthlySalary()
         {
             return hWorked * hourlyRate;
